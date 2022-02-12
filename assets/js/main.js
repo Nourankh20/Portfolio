124f871956fc263ca30f678148192fdf960707ed
Nourankh20 <82681541+Nourankh20@users.noreply.github.com>
2022-02-12 16:37:20 +0200

    Changed colours and dimensions, added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,4 +64,14 @@ if (selectedTheme){
 
   })
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset:true
+})
 
+sr.reveal('.home__data')
+sr.reveal('.home__handle', {delay: 700})
+sr.reveal(`.home__social , .home__scroll`, {delay: 700, origin:'buttom'})
